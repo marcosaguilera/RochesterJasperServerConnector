59a495ac1c503b6037b9e8f9c5efdae773b8ffa6
Marcos Aguilera Ely <maguilera@rochester.edu.co>
2015-03-20 10:58:10 -0500

    JasperServer WebService to download reports :: Rochester School Test

---
+++ src/test/java/com/gkudos/jasperserver/client/JasperserverRestClientTest.java
@@ -0,0 +1,122 @@
+package com.gkudos.jasperserver.client;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author juanmendez@gkudos.com
+ * 
+ */
+
+public class JasperserverRestClientTest {
+	public static final Logger LOGGER = LoggerFactory.getLogger(JasperserverRestClientTest.class);
+	
+	/*private final static String serverUrl = "http://myserver/jasperserver/";
+	private final static String serverUser = "joeuser";
+	private final static String serverPassword = "joeuser";*/
+        
+        private final static String serverUrl = "http://leon:8090/jasperserver/";
+	private final static String serverUser = "jasperadmin";
+	private final static String serverPassword = "jasperadmin";
+	
+	private File outPutDir;
+	
+	@BeforeTest
+	public void beforeTest() {
+		LOGGER.debug("beforeTest...");
+		outPutDir = new File(System.getProperty("java.io.tmpdir"));
+                LOGGER.debug("--> File location: "+outPutDir);
+		assertNotNull(outPutDir);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		LOGGER.debug("afterTest...");
+	}
+	
+
+	/**
+	 * Ejecuta el reporte y retorna un archivo
+	 **/
+	@Test
+	public void testGetReportAsFile() {
+		LOGGER.debug("testGetReportAsFile");
+		try {
+			Report report = new Report();
+			//report.setUrl("/reports/samples/Employees");
+                        report.setUrl("/aprendozreports/EXT001");
+			report.setOutputFolder(outPutDir.getAbsolutePath());
+			LOGGER.info(report.toString());
+			JasperserverRestClient client = JasperserverRestClient.getInstance(serverUrl, serverUser, serverPassword);
+			File reportFile  = client.getReportAsFile(report);
+			assertNotNull(reportFile);
+			LOGGER.debug("reportFile:"+reportFile.getAbsolutePath());
+		} catch (Exception e) {
+			fail(e.getMessage(), e);
+		}
+	}
+	
+	/*@Test
+	public void testGetBigReportAsFile() {
+		LOGGER.debug("testGetBigReportAsFile");
+		try {
+			Report report = new Report();
+			report.setUrl("/reports/samples/SalesByMonth");
+			report.setOutputFolder(outPutDir.getAbsolutePath());
+			LOGGER.info(report.toString());
+			JasperserverRestClient client = JasperserverRestClient.getInstance(serverUrl, serverUser, serverPassword);
+			File reportFile = client.getReportAsFile(report);
+			assertNotNull(reportFile);
+			LOGGER.debug("reportFile:"+reportFile.getAbsolutePath());
+		} catch (Exception e) {
+			fail(e.getMessage(), e);
+		}
+	}*/
+	
+	@Test
+	public void testGetBigReportAsExcelFile() {
+		LOGGER.debug("testGetBigReportAsExcelFile");
+		try {
+			Report report = new Report();
+			report.setFormat(Report.FORMAT_EXCEL);
+			//report.setUrl("/reports/samples/SalesByMonth");
+                        report.setUrl("/aprendozreports/EXT001");
+			report.setOutputFolder(outPutDir.getAbsolutePath());
+			LOGGER.info(report.toString());
+			JasperserverRestClient client = JasperserverRestClient.getInstance(serverUrl, serverUser, serverPassword);
+			File reportFile = client.getReportAsFile(report);
+			assertNotNull(reportFile);
+			LOGGER.debug("reportFile:"+reportFile.getAbsolutePath());
+		} catch (Exception e) {
+			fail(e.getMessage(), e);
+		}
+	}
+	
+	/*@Test
+	public void testGetReportWithParamsAsFile() {
+		LOGGER.debug("testGetReportWithParamsAsFile");
+		try {
+			Report report = new Report();
+			report.setUrl("/reports/samples/Department");
+			report.setOutputFolder(outPutDir.getAbsolutePath());
+			report.addParameter("department", "11");
+			LOGGER.info(report.toString());
+			JasperserverRestClient client = JasperserverRestClient.getInstance(serverUrl, serverUser, serverPassword);
+			File reportFile = client.getReportAsFile(report);
+			assertNotNull(reportFile);
+			LOGGER.debug("reportFile:"+reportFile.getAbsolutePath());
+		} catch (Exception e) {
+			fail(e.getMessage(), e);
+		}
+	}*/
+
+}
